b5bc3525a9165bdd41fd0dca5c5820c46dc89e0d
ma.k.goncharova <ma.k.goncharova@tbank.ru>
2025-12-08 17:30:45 +0300

    day 7 done

--- src/day7/task.ts
+++ src/day7/task.ts
@@ -1,85 +1,100 @@
 import {example, input} from "./input";
 import {prepareArrayInput, prepareInput} from "../../utils/prepareInput";
 
-const transpose = (input: string[][]) => {
-    const transposed: string[][] = [];
-    for (let i = 0; i < input.length; i++) {
-        for (let j = 0; j < input[i].length; j++) {
-            if (!transposed[j]) transposed[j] = [];
-            transposed[j][i] = input[i][j];
+type Point = { x: number; y: number };
+type Cache = { [key: string]: boolean };
+
+const getKey = (point: Point) => `${point.x},${point.y}`;
+
+const checkUp = (point: Point, arr: string[][], cache: Cache): boolean => {
+    const key = getKey(point);
+
+    if (!arr[point.y]?.[point.x]) return false;
+    if (arr[point.y][point.x] === 'S') return true
+    if (arr[point.y][point.x] === '^' && arr[point.y][point.x - 1] !== '^' && arr[point.y][point.x + 1] !== '^') return false;
+    if (arr[point.y][point.x - 1] === '^' || arr[point.y][point.x + 1] === '^') return checkUp({
+        x: point.x - 1,
+        y: point.y - 1
+    }, arr, cache) || checkUp({x: point.x + 1, y: point.y - 1}, arr, cache);
+    return checkUp({x: point.x, y: point.y - 1}, arr, cache);
+
+}
+const goDown = (point: Point, arr: string[][], sum: number, cache: Cache): void => {
+    if (point.x < 0 || point.x > arr[0].length - 1) return;
+    if (point.y > arr.length - 1) return;
+    const key = getKey(point)
+    const pointSym = arr[point.y][point.x];
+
+    if (pointSym === '.') goDown({x: point.x, y: point.y + 1}, arr, sum, cache);
+    if (pointSym === '^') {
+        if (key in cache) return;
+        cache[key] = true;
+        goDown({x: point.x - 1, y: point.y + 1}, arr, sum, cache);
+        goDown({
+            x: point.x + 1,
+            y: point.y + 1
+        }, arr, sum, cache)
+    }
+    return;
+}
+
+const goDown2 = (point: Point, arr: string[][], sum: number, cache: Cache): number => {
+    if (point.x < 0 || point.x > arr[0].length - 1) return 1;
+    if (point.y > arr.length - 1) return 1;
+    const key = getKey(point)
+    const pointSym = arr[point.y][point.x];
+
+    if (pointSym === '.') return goDown2({x: point.x, y: point.y + 1}, arr, sum, cache);
+    if (pointSym === '^') {
+        if (key in cache) return cache[key];
+        const res = goDown2({x: point.x - 1, y: point.y + 1}, arr, sum, cache) + goDown2({
+            x: point.x + 1,
+            y: point.y + 1
+        }, arr, sum, cache)
+        cache[key] = res;
+        return res
+    }
+    return sum;
+}
+
+
+
+
+const findResult1 = (arr: string[][]) => {
+    let start = {x: 0, y: 0};
+    const cache = {}
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length; j++) {
+            if (arr[i][j] === 'S') {
+                start.x = j;
+                start.y = i;
+
+
+            }
         }
     }
-    return transposed
-}
-
-const processInput = (input: string) => {
-    const arr: string[][] = input.split('\n').map((line) => line.split(/\s+/).filter(item => !!item));
-
-    return transpose(arr);
-}
-
-const processInput2 = (input: string) => {
-    const arr = input.split('\n');
-    const firstLine = arr[0];
-    const matrix: string[][] = [];
-    let start = 0;
-    for (let i = 0; i < firstLine.length; i++) {
-        if (firstLine[i] === ' ') {
-            let allSpace = true;
-            for (let j = 0; j < arr.length; j++) {
-                if (arr[j][i] !== ' ') allSpace = false;
-            }
-            if (allSpace) {
-                for (let j = 0; j < arr.length; j++) {
-                    if (!matrix[j]) matrix[j] = [];
-                    matrix[j].push(arr[j].substring(start, i));
-                }
-                start = i + 1;
-            }
-        }
-    }
-    for (let j = 0; j < arr.length; j++) { //для последнего рядя
-        if (!matrix[j]) matrix[j] = [];
-        matrix[j].push(arr[j].substring(start));
-    }
-
-    return transpose(matrix);
-
-}
-
-
-const findResult = (arr: string[][]) => {
-    return arr.reduce((acc, line) => {
-        const sign = line[line.length - 1];
-        line.pop();
-        return acc + line.reduce((acc, num) => {
-            return sign === '*' ? (+acc) * (+num) : (+acc) + (+num);
-        })
-    }, 0)
+    goDown({x: start.x, y: start.y + 1}, arr, 0, cache)
+    return Object.keys(cache).length;
 }
 
 const findResult2 = (arr: string[][]) => {
-    return arr.reduce((acc, line) => {
-        let max = 0;
-        const sign = line[line.length - 1].trim();
-        line.pop();
-        for (let i = 0; i < line.length; i++) {
-            if (line[i].length > max) max = line[i].length;
+    let start = {x: 0, y: 0};
+    const cache = {}
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length; j++) {
+            if (arr[i][j] === 'S') {
+                start.x = j;
+                start.y = i;
+
+
+            }
         }
-        const newNums = [];
+    }
 
-
-        for (let j = 0; j < max; j++) {
-            newNums.push(line.map(item => item[j] ?? '').join(''));
-        }
-
-        return acc + newNums.reduce((acc, item) => {
-            return sign === '*' ? +acc * +item : +acc + +item;
-        })
-
-    }, 0)
+    return goDown2({x: start.x, y: start.y + 1}, arr, 0, cache)
 }
 
-const data = processInput2(input)
+
+const data = prepareArrayInput(input)
 console.log(findResult2(data));
 
